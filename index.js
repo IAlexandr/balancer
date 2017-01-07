524255532c4161cc095f46b6ac5d34833ce1fe7d
Alexandr Ivanov <aivanov@informatica.ru>
2017-01-07 15:33:49 +0300

    logger

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // TODO запуск пингеров по сервисам (etcd, s3, rabbitmq, pg)
 // TODO модуль поднятия новых виртуальных машин.
 // TODO журналирование
+
+import dg from 'debug';
+const debug = dg('balancer');
+import logger from './lib/logger';
+
+debug('Initializing.');
+logger();
---
+++ options.js
@@ -0,0 +1,61 @@
+const version = require('./package.json').version;
+const optionsSpec = {
+  PORT: {
+    required: true,
+    default: '4567',
+    env: 'BALANCER_PORT'
+  },
+  ETCD_CONN: {
+    required: true,
+    default: 'http://10.157.212.14:2379',
+    env: 'BALANCER_ETCD_CONN'
+  },
+  RESTART_TO: {
+    required: true,
+    default: '10000', // ms
+    env: 'BALANCER_ETCD_CONN'
+  },
+  PROOF_ALIVE_TO: {
+    required: true,
+    default: '15000', // ms
+    env: 'BALANCER_ETCD_CONN'
+  },
+  BALANCER_ID: {
+    required: true,
+    default: 'balancer_id1',
+    env: 'BALANCER_ID'
+  },
+  IS_MASTER: {
+    required: true,
+    default: 'false',
+    env: 'BALANCER_IS_MASTER'
+  }
+};
+
+let options = {
+  version
+};
+
+const op = {
+  ...options, ...Object.keys(optionsSpec).map((key) => {
+    if (!optionsSpec[key].preprocess) {
+      optionsSpec[key].preprocess = function preprocess (str) {
+        return str;
+      };
+    }
+    const opt = { name: key };
+    if (process.env[optionsSpec[key].env]) {
+      opt.value = optionsSpec[key].preprocess(process.env[optionsSpec[key].env]);
+    } else if (optionsSpec[key].default) {
+      opt.value = optionsSpec[key].preprocess(optionsSpec[key].default);
+    } else if (optionsSpec[key].required) {
+      throw new Error('!!! REQUIRED OPTION NOT SET: ' + key);
+    }
+    return opt;
+  }).reduce((prev, cur) => {
+    prev[cur.name] = cur.value;
+    return prev;
+  }, {})
+};
+
+export default op;
